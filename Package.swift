50b814feaed5207dc4a67eb9f270e8d08a0d3dac
Shmeel Ahmed <shmeel.ahmed@systemsltd.com>
2023-06-08 22:36:03 +0500

    dependancies removed

--- Package.swift
+++ Package.swift
@@ -5,22 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "AppHeader",
-    products: [
+    platforms: [
+        .iOS(.v13)
+    ],products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AppHeader",
             targets: ["AppHeader"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/smilesiosteam/LottieAnimationManager.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/smilesiosteam/SmilesLocationHandler.git", .upToNextMajor(from: "1.0.0")),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppHeader",
-            dependencies: []),
+            dependencies: [
+                .product(name: "LottieAnimationManager", package: "LottieAnimationManager"),
+                .product(name: "SmilesLocationHandler", package: "SmilesLocationHandler"),
+            ]),
         .testTarget(
             name: "AppHeaderTests",
             dependencies: ["AppHeader"]),
--- Sources/AppHeader/AppHeader.swift
+++
@@ -1,6 +0,0 @@
-public struct AppHeader {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
