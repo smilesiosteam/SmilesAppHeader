0143f364b51e074425914e49610aba20874c6ecb
Habib Rehman <haburrehman@eand.com>
2023-10-12 11:43:19 +0500

    -added LocationHandler branch from main to shared_Main.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/smilesiosteam/LottieAnimationManager.git", branch: "main"),
-        .package(url: "https://github.com/smilesiosteam/SmilesLocationHandler.git", branch: "main")
+        .package(url: "https://github.com/smilesiosteam/SmilesLocationHandler.git", branch: "shared_Models")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
