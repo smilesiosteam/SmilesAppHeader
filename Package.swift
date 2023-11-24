6f4cead064fd2008b439c05eb74289a3f2104021
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-11-24 16:04:22 +0500

    - Pointed location handler to address_book branch - Renamed entry point param

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/smilesiosteam/LottieAnimationManager.git", branch: "main"),
-        .package(url: "https://github.com/smilesiosteam/SmilesLocationHandler.git", branch: "main")
+        .package(url: "https://github.com/smilesiosteam/SmilesLocationHandler.git", branch: "address_book")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
